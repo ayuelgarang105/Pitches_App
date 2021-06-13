51b2022e398a6c00a15d6a87937110a45f6de2f7
Gabriel Ayuel <ayuelgarang105@gmail.com>
2021-06-13 22:08:18 +0300

    modify files

--- app/email.py
+++ app/email.py
@@ -10,7 +10,7 @@ subject_pref = 'k-pitch'
 
 def mail_message(subject,template,to,**kwargs):
    
-    sender_email = 'kerrykomar@gmail.com'
+    sender_email = 'ayuelgarang105@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
