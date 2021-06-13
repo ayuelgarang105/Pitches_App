df2100b5e96ecc2542a1a210dc3ad1a1f1a58afb
Gabriel Ayuel <ayuelgarang105@gmail.com>
2021-06-13 22:06:05 +0300

    add config

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gabriel:gazajoga@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+    #email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SENDER_EMAIL = 'ayuelgarang105@gmail.com'
+
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+pass
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gabriel:gazajoga@localhost/pitch'
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gabriel:gazajoga@localhost/pitch'
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
